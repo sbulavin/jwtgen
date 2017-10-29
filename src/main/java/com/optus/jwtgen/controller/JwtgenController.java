6701f69795a329a9f6f98fa104cfd7c4a4f74abf
s.bulavin <s.bulavin@ukr.net>
2017-10-29 18:32:41 +0200

    generate token part 2

--- src/main/java/com/optus/jwtgen/controller/JwtgenController.java
+++ src/main/java/com/optus/jwtgen/controller/JwtgenController.java
@@ -1,7 +1,14 @@
 package com.optus.jwtgen.controller;
 
+import com.optus.jwtgen.request.JwtPayload;
+import com.optus.jwtgen.service.JwtgenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 /**
  * Created by optus on 28.10.17.
@@ -10,8 +17,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class JwtgenController {
 
+    @Autowired
+    JwtgenService jwtgenService;
+
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "Greetings from JWT generator!";
+    }
+
+    @RequestMapping(value = "/token", method = RequestMethod.POST)
+    public String getToken(@Valid @RequestBody JwtPayload payload){
+        try {
+            return jwtgenService.generateToken(payload);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return e.getMessage();
+        }
     }
 }
--- src/main/java/com/optus/jwtgen/service/JwtgenService.java
+++ src/main/java/com/optus/jwtgen/service/JwtgenService.java
@@ -1,11 +1,43 @@
 package com.optus.jwtgen.service;
 
-/**
- * Created by optus on 28.10.17.
- */
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.optus.jwtgen.request.JwtPayload;
+import com.optus.jwtgen.utils.PemUtils;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.net.URL;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.util.Date;
+
+
+
+@Component
 public class JwtgenService {
 
-    public String generateToken(String payload){
-        return null;
+    public String generateToken(JwtPayload payload) throws IOException {
+
+        URL fileLocation = JwtgenService.class.getResource("/keys/rsaPublicKey.pem");
+        RSAPublicKey publicKey = (RSAPublicKey) PemUtils.readPublicKeyFromFile(fileLocation.getPath(), "RSA");
+
+        fileLocation = JwtgenService.class.getResource("/keys/rsaPrivateKey.pem");
+        RSAPrivateKey privateKey = (RSAPrivateKey) PemUtils.readPrivateKeyFromFile(fileLocation.getPath(), "RSA");
+
+        Algorithm algorithm = Algorithm.RSA256(publicKey, privateKey);
+        String token = JWT.create()
+                .withNotBefore(new Date(payload.getNbf() * 1000))
+                .withExpiresAt(new Date(payload.getExp() * 1000))
+                .withIssuedAt(new Date(payload.getIat() * 1000))
+                .withAudience(payload.getAud())
+                .withJWTId(payload.getJti())
+                .withClaim("emp", payload.getEmp())
+                .withClaim("bra", payload.getBra())
+                .withClaim("ini", payload.getIni())
+                .withClaim("abt", payload.getAbt())
+                .sign(algorithm);
+
+        return token;
     }
 }
